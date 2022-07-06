de67e501b613a5261fc9cf942a52a40136b45f1f
Jeff <jeff@Jeffs-MacBook-Pro.local>
2022-07-06 21:18:05 +0400

    change web

--- mirrors/.resource/js/helptool.js
+++
@@ -1,4 +0,0 @@
-// 动态修改选择内容
-function updateContent(clsName, content) {
-    $(clsName).html(content);
-}
--- mirrors/.resource/js/main.js
+++
@@ -1,4 +0,0 @@
-// 启动镜像前端数据同步
-jQuery(document).ready(function($) {
-    mirror.init();
-});
